96249cd3164e16fcd62af2bfd89d5080401b658f
Ronaldo Kipkirui <ronaldochirchir8@gmail.com>
2024-12-04 21:19:10 +0300

    completed assignment

---
+++ scuber.js
@@ -0,0 +1,16 @@
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+  function setBestCustomer() {
+    var bestCustomer = 'not bob';
+  }
+  function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+  }
+  const leastFavoriteCustomer = 'Alice';
+
+function changeLeastFavoriteCustomer() {
+  
+  leastFavoriteCustomer = 'Bob';
+}
